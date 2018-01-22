4478eb9ee908a86c5fa1f1a3d617dcfbfc562529
Beetix <b.t.freeman16@gmail.com>
2018-01-22 08:44:26 +0100

    Pepper says the periodic element when clicked
    - Using QiMessaging
    - Translated elements to french

---
+++ README.md
@@ -0,0 +1,4 @@
+Periodic Table for Pepper
+=========================
+
+Three JS periodic table example for Pepper
---
+++ app.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- encoding: UTF-8 -*-
+
+
+import qi
+import argparse
+import sys
+import time
+import signal
+
+def signal_handler(signal, frame):
+        print('Bye!')
+        sys.exit(0)
+
+def main(session):
+    # Get the service ALTabletService.
+
+    try:
+        tabletService = session.service("ALTabletService")
+
+        tabletService.loadApplication("periodic_table")
+        tabletService.showWebview()
+
+        print("Waiting for Ctrl+C to disconnect")
+        signal.pause()
+
+    except Exception, e:
+        print "Error was: ", e
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="127.0.0.1",
+                        help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Naoqi port number")
+
+    args = parser.parse_args()
+    session = qi.Session()
+    try:
+        session.connect("tcp://" + args.ip + ":" + str(args.port))
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+    main(session)
+
